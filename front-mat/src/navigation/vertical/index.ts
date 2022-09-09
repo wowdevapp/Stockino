328ad317d2bd51ac0d3b75eb22162289a9fa1b60
abdelhaq fouad <abdelhaq.fouad@omnevo.net>
2022-09-09 23:35:47 +0100

    figured out best choice on front

---
+++ front-mat/src/navigation/vertical/index.ts
@@ -0,0 +1,65 @@
+// ** Icon imports
+import Table from 'mdi-material-ui/Table'
+import ChartDonut from 'mdi-material-ui/ChartDonut'
+import FormSelect from 'mdi-material-ui/FormSelect'
+import CubeOutline from 'mdi-material-ui/CubeOutline'
+import LockOutline from 'mdi-material-ui/LockOutline'
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import EmailOutline from 'mdi-material-ui/EmailOutline'
+import ShieldOutline from 'mdi-material-ui/ShieldOutline'
+import AccountOutline from 'mdi-material-ui/AccountOutline'
+import ArchiveOutline from 'mdi-material-ui/ArchiveOutline'
+import DotsHorizontal from 'mdi-material-ui/DotsHorizontal'
+import MessageOutline from 'mdi-material-ui/MessageOutline'
+import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
+import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
+import VectorArrangeBelow from 'mdi-material-ui/VectorArrangeBelow'
+import FileDocumentOutline from 'mdi-material-ui/FileDocumentOutline'
+import CalendarBlankOutline from 'mdi-material-ui/CalendarBlankOutline'
+import PackageVariantClosed from 'mdi-material-ui/PackageVariantClosed'
+import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import CheckboxMarkedCircleOutline from 'mdi-material-ui/CheckboxMarkedCircleOutline'
+
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Dashboards',
+      icon: HomeOutline,
+      badgeContent: 'new',
+      badgeColor: 'error',
+      children: [
+        {
+          title: 'CRM',
+          path: '/dashboards/crm'
+        },
+        {
+          title: 'Analytics',
+          path: '/dashboards/analytics'
+        },
+        {
+          title: 'eCommerce',
+          path: '/dashboards/ecommerce'
+        }
+      ]
+    },
+    {
+      title: 'User',
+      icon: AccountOutline,
+      children: [
+        {
+          title: 'List',
+          path: '/apps/user/list'
+        },
+        {
+          title: 'View',
+          path: '/apps/user/view'
+        }
+      ]
+    },
+  ]
+}
+
+export default navigation
---
+++ front-mat/src/pages/dashboards/crm/index.tsx
@@ -0,0 +1,39 @@
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+
+// ** Icon Imports
+import CartPlus from 'mdi-material-ui/CartPlus'
+import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
+
+// ** Custom Component Import
+import CardStatisticsVertical from 'src/@core/components/card-statistics/card-stats-vertical'
+
+// ** Styled Component Import
+import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
+
+// ** Demo Components Imports
+import CrmAward from 'src/views/dashboards/crm/CrmAward'
+import CrmTable from 'src/views/dashboards/crm/CrmTable'
+import CrmTotalGrowth from 'src/views/dashboards/crm/CrmTotalGrowth'
+import CrmTotalProfit from 'src/views/dashboards/crm/CrmTotalProfit'
+import CrmMonthlyBudget from 'src/views/dashboards/crm/CrmMonthlyBudget'
+import CrmExternalLinks from 'src/views/dashboards/crm/CrmExternalLinks'
+import CrmWeeklyOverview from 'src/views/dashboards/crm/CrmWeeklyOverview'
+import CrmPaymentHistory from 'src/views/dashboards/crm/CrmPaymentHistory'
+import CrmOrganicSessions from 'src/views/dashboards/crm/CrmOrganicSessions'
+import CrmProjectTimeline from 'src/views/dashboards/crm/CrmProjectTimeline'
+import CrmMeetingSchedule from 'src/views/dashboards/crm/CrmMeetingSchedule'
+import CrmSocialNetworkVisits from 'src/views/dashboards/crm/CrmSocialNetworkVisits'
+import CrmMostSalesInCountries from 'src/views/dashboards/crm/CrmMostSalesInCountries'
+
+const CrmDashboard = () => {
+  return (
+    <ApexChartWrapper>
+      <Grid container spacing={6} className='match-height'>
+
+      </Grid>
+    </ApexChartWrapper>
+  )
+}
+
+export default CrmDashboard
